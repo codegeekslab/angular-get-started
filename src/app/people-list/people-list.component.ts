2ffe9d1ad1ae7a8c40855aeb011e961b648ca68f
saurabh <saurabh.kumar@epsilon.com>
2018-08-07 23:13:51 +0530

    end of two-it displays list of names

---
+++ src/app/people-list/people-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Person } from '../person';
+import { PeopleService } from "../people.service";
+
+@Component({
+  selector: 'app-people-list',
+  template: `
+  <!-- this is the new syntax for ng-repeat -->
+  <ul>
+    <li *ngFor="let person of people">
+     {{person.name}}
+    </li>
+  </ul>
+  `,
+  styleUrls: ['./people-list.component.scss']
+})
+export class PeopleListComponent implements OnInit {
+  people: Person[] = []
+  constructor(private _peopleService: PeopleService) {}
+
+  ngOnInit() {
+    this.people = this._peopleService.getAll()
+  }
+}
